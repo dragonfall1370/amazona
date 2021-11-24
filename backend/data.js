f3f0be048ea898fb407bbe93afccf4b9728e9cad
dragonfall1370 <g.dragonfall1370@gmail.com>
2021-11-24 17:16:55 +0700

    add backend

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [{
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+            countInStock: 10
+        },
+        {
+            _id: '2',
+            name: 'Addidas Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            brand: 'Addidas',
+            rating: 4.0,
+            numReviews: 3,
+            description: 'high quality product',
+            countInStock: 17
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 120,
+            brand: 'Lacoste',
+            rating: 3.0,
+            numReviews: 10,
+            description: 'high quality product',
+            countInStock: 0
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 160,
+            brand: 'Nike',
+            rating: 5.0,
+            numReviews: 1,
+            description: 'high quality product',
+            countInStock: 4
+        },
+        {
+            _id: '5',
+            name: 'Cholon Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 200,
+            brand: 'Cholon',
+            rating: 2.0,
+            numReviews: 10,
+            description: 'high quality product',
+            countInStock: 20
+        },
+        {
+            _id: '6',
+            name: 'benthanh Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p6.jpg',
+            price: 700,
+            brand: 'benthanh',
+            rating: 0.5,
+            numReviews: 100,
+            description: 'poor quality product',
+            countInStock: 5
+        }
+    ]
+};
+
+export default data;
