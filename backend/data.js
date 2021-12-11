b197432a7f164ce06f73dda3f777acd8fa1d4c02
dragonfall1370 <g.dragonfall1370@gmail.com>
2021-12-11 15:59:16 +0700

    first mongodb express

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+        {
+            name:'Basir',
+            email:'admin@example.com',
+            password:bcrypt.hashSync('1234', 8),
+            isAdmin:true,
+        },
+        {
+            name:'John',
+            email:'John.User@example.com',
+            password:bcrypt.hashSync('12345', 8),
+            isAdmin:false,
+        }
+    ],
     products: [{
             _id: '1',
             name: 'Nike Slim Shirt',
