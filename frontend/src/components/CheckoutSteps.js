377e7f7060b0683b8318932c7a2848a273b4dad7
dragonfall1370 <g.dragonfall1370@gmail.com>
2021-12-27 21:36:08 +0700

    add payment

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -10,7 +10,7 @@ export default function checkoutSteps(props) {
                 Shipping
             </div>
             <div className={props.step3 ? 'active': ''}>
-                payment
+                Payment
             </div>
             <div className={props.step4 ? 'active': ''}>
                 Place Order
