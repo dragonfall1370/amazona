4a7584b2fb4ec95d319742e1e126ef6ca264c098
dragonfall1370 <g.dragonfall1370@gmail.com>
2022-01-26 10:36:52 +0700

    order

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -27,13 +28,15 @@ app.use('/api/users', userRouter);
 
 app.use('/api/products', productRouter);
 
+app.use('/api/orders', orderRouter);
+
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
 app.use((err, req, res, next) =>{
     res.status(500).send({ message:err.message});
-})
+});
 
 
 const port = process.env.PORT || 5000;
